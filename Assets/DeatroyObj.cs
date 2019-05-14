51912fc5d5c947c304f28888d324760b503a72da
Shutaro.K <gijhide@gmail.com>
2019-05-14 10:43:58 +0900

    [add]Syateki内のファイルを追加

---
+++ Assets/DeatroyObj.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class DeatroyObj : MonoBehaviour {
+	public int scoreValue;  // これが敵を倒すと得られる点数になる
+	private ScoreManager sm;
+
+	// ★追加
+	void Start(){
+
+		// 「ScoreManagerオブジェクト」に付いている「ScoreManagerスクリプト」の情報を取得して「sm」の箱に入れる。
+		sm = GameObject.Find ("ScoreManager").GetComponent<ScoreManager> ();
+	}
+	// このメソッドはぶつかった瞬間に呼び出される
+	void OnTriggerEnter(Collider other){
+
+		// もしもぶつかった相手のTagにShellという名前が書いてあったならば（条件）
+		if(other.CompareTag("Bullet")){
+
+			// このスクリプトがついているオブジェクトを破壊する（thisは省略が可能）
+			Destroy(this.gameObject);
+
+			// ぶつかってきたオブジェクトを破壊する
+			Destroy(other.gameObject);
+
+			sm.AddScore(scoreValue);
+		}
+	}
+}
---
+++ Assets/Shooting.cs
@@ -0,0 +1,51 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class Shooting : MonoBehaviour {
+
+	// bullet prefab
+	public GameObject bullet;
+
+	// 弾丸発射点
+	public Transform muzzle;
+
+	// 弾丸の速度
+	public float speed = 1000;
+
+	public int shotCount = 8;
+
+	public Text BulletLabel;
+
+	// Use this for initialization
+	void Start () {
+		// ★追加
+			BulletLabel.text = "残り弾数：" + shotCount;
+	}
+
+	// Update is called once per frame
+	void Update () {
+		// 左クリックされた時
+		if(Input.GetMouseButtonDown(0)){
+			if(shotCount < 1){
+				return;
+			}
+			// ★追加
+			shotCount -= 1;
+
+			// ★追加
+			BulletLabel.text = "残り弾数：" + shotCount;
+			// 弾丸の複製
+			GameObject bullets = GameObject.Instantiate(bullet)as GameObject;
+
+			Vector3 force;
+			force = this.gameObject.transform.forward * speed;
+			// Rigidbodyに力を加えて発射
+			bullets.GetComponent<Rigidbody>().AddForce (force);
+			// 弾丸の位置を調整
+			bullets.transform.position = muzzle.position;
+			bullets.transform.rotation = muzzle.rotation;
+		}
+	}
+}
